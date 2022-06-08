6cef9a8d9fc05fe2c7e606a3de6343cf8ab21053
isaac mariga <isaacwangombe5@gmail.com>
2022-06-08 18:55:04 +0300

    Adding models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,31 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Projects(models.Model):
+  title = models.CharField(max_length =30)
+  image = models.ImageField(upload_to = '/project_images')
+  description = models.TextField(max_length =300)
+  link = models.URLField(max_length=200)
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.title
+
+  def save_table(self):
+  	  self.save()
+
+
+class Profile(models.Model):
+  picture = models.ImageField(upload_to = '/profile_images')
+  bio = models.TextField(max_length =300)
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.id
+
+  def save_table(self):
+  	  self.save()
