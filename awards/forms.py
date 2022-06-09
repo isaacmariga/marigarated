a299823d53b9179efc81a2d1bebfded6b6f4b4d8
isaac mariga <isaacwangombe5@gmail.com>
2022-06-09 13:52:23 +0300

    Addinf review form

--- awards/forms.py
+++ awards/forms.py
@@ -1,12 +1,20 @@
 from django import forms
-from .models import Profile, Projects
+from .models import Profile, Projects, Review
 
 
 class ProjectForm(forms.ModelForm):
   class Meta:
     model = Projects
     exclude = ['user']
+
+
 class ProfileForm(forms.ModelForm):
   class Meta:
     model = Profile
     exclude = ['user']
+
+    
+class ReviewForm(forms.ModelForm):
+  class Meta:
+    model = Review
+    exclude = ['user', 'project']
