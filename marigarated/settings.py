2665f42982af8ba9c44d9e4bfdc53860c8a525bc
isaac mariga <isaacwangombe5@gmail.com>
2022-06-11 19:38:37 +0300

    deployment

--- marigarated/settings.py
+++ marigarated/settings.py
@@ -46,7 +46,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
