4b321e2bd54d515ea9608408cb9926ea2a58454f
isaac mariga <isaacwangombe5@gmail.com>
2022-06-15 12:37:01 +0300

    deployment

--- marigarated/settings.py
+++ marigarated/settings.py
@@ -177,3 +177,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
